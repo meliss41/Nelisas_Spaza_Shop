024297c0a7bfc118681ad9a8ddf88f47490b8a09
oyama <oyama@projectcodex.co>
2015-06-23 11:05:54 +0200

    test 1 is running still need to get test 2 running

---
+++ test/Nelisa_spazaSales_tests.js
@@ -0,0 +1,22 @@
+var assert = require("assert");
+var productsSold = require('../modules/productsSold');
+
+describe("Nelisa's spaza sales processing", function(){
+
+    it('should find all the sales rows in the file', function(){       
+        assert.equal(448, productsSold.findSalesRows('./files/NelisaSalesHistory.csv').length);
+    });
+
+    it('should find the most popular product', function(){
+
+        //think how to find most popular product...
+
+        //you might need some other functions to help you...
+
+        var mostPopularProduct = productsSold.findMostPopularProduct();
+        assert.equal(mostPopularProduct, "Prod2");
+
+    });
+
+    
+});
