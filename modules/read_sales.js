8133643f839e999b688913e0ab6b88dda4589732
oyama <oyama@projectcodex.co>
2015-07-02 11:57:36 +0200

    still need to know how would we reuse getsales function and also to use a proper mock object to master the tests

--- modules/read_sales.js
+++ modules/read_sales.js
@@ -6,18 +6,9 @@ module.exports = function(fileName){
         var fileContent = fs.readFileSync(fileName, "utf8");//read files in folder
         
         //split the file into rows
-<<<<<<< HEAD
         var lines = fileContent.split('\r').splice(1);
         var productsSold = [];
         lines.forEach(function(line){
-=======
-        var lines = fileContent.split('\r').splice(1); 
-        var productsSold = [];     
-            lines.forEach(function(line){
-            var hold = line.split(',');
-            var productName = hold[2];
-            var quantitySold = hold[3];
->>>>>>> 2fdee6771da44f2577a54e0784e4b711c470a70a
             //split each line into fields
         var fields = line.split(";");
         productsSold.push(fields);
@@ -30,13 +21,3 @@ module.exports = function(fileName){
             
     }
 }
-        productsSold.forEach(function(){
-            
-        if (productMap[productName]=== undefined) {
-                productNames.push(productName);
-                productMap[productName]= 0;
-
-                console.log(productNames);
-        }
-
-    });
