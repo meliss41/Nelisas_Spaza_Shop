d7ab3bf20d29f1c47035202faa778e2142ccbfbf
oyama <oyama@projectcodex.co>
2015-07-06 15:06:11 +0200

    finally both functions are working + tests

--- test/Nelisa_spazaSales_tests.js
+++ test/Nelisa_spazaSales_tests.js
@@ -15,27 +15,9 @@ describe("Nelisa's spaza sales processing", function(){
 
     it('should find the most popular product', function(){
 
-        //think how to find most popular product...
-
-        // you might need some other functions to help you...        
-       
-        // Mock object
-        var reader = {
-        	findMostPopularProduct : function(){
-        	return [  
-        		[ 'Sunday', '1-Mar', 'Apples', '1', 'R2,00' ],
-        		[ 'Sunday', '1-Mar', 'Pears', '5', 'R2,00' ],
-        		[ 'Sunday', '1-Mar', 'Apples', '4', 'R2,00' ],
-        		[ 'Sunday', '1-Mar', 'Pears', '5', 'R2,00' ]
-        	]}
-        };
-        ;
-         var mostPopular = require('../modules/Nelisa_Spaza_Sales');
-         var reader = mostPopular.findMostPopularProduct('./files/NelisaSalesHistory.csv');
-        assert.equal({"productName":"Mixed Sweets 5s","qty":172},reader);
-        // assert.equal(reader,{productName : "Mixed Sweets 5s", qty : 172});
-        // var mostPopularProduct = productsSold.findMostPopularProduct();
-        // assert.equal(productsSold.findSalesRows('./files/NelisaSalesHistory.csv'));
+        var mostPopular =  productsSold;
+        var reader = mostPopular.findMostPopularProduct('./files/NelisaSalesHistory.csv');
+         assert.deepEqual({"productName":"Mixed Sweets 5s","qty":172},reader);
     });
 
     
