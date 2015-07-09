66c48beff0d53021d5f1447b0c76e44079601309
oyama <oyama@projectcodex.co>
2015-07-09 14:16:30 +0200

    tests are passing story 3 is now done

--- test/Nelisa_spazaSales_tests.js
+++ test/Nelisa_spazaSales_tests.js
@@ -26,10 +26,16 @@ describe("Nelisa's spaza sales processing", function(){
              assert.deepEqual(salesPerProduct, {"categoryName" : "Short_Life_Groceries", "qty" : 397}); 
 
         });
+
+    it('should find least popular product name plus qty', function(){
+            var salesPerProduct = productsSold.findLeastPopularProduct('./files/NelisaSalesHistory.csv');
+             assert.deepEqual(salesPerProduct,{"productName":"Rose (plastic)","qty":14} ); 
+
+        });
+
     it('should find least popular category name plus qty', function(){
             var salesPerProduct = productsSold.findLeastPopularCategory('./files/NelisaSalesHistory.csv');
              assert.deepEqual(salesPerProduct, {"categoryName" : "Novelty_Goods", "qty" : 28}); 
 
         });
-        
 });
