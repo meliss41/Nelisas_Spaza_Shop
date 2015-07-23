9bb726f97ec0fc54760d802740e68726a07acf7b
oyama <oyama@projectcodex.co>
2015-07-23 16:09:21 +0200

    tweaked code sales module is running good

--- modules/Nelisa_Spaza_Sales.js
+++ modules/Nelisa_Spaza_Sales.js
@@ -4,175 +4,187 @@ var salesAccessories = require('./Nelisa_Spaza_Sales_earnings');
 
 exports.findMostPopularProduct = function(salesFileReader){
 
-      var sales = salesFileUtilities.getSales("./files/NelisaSalesHistory.csv");
-      var productsSold = salesFileUtilities.getQtyPerProduct(sales);
+    var sales = salesFileUtilities.getSales("./files/NelisaSalesHistory.csv");
+    var productsSold = salesFileUtilities.getQtyPerProduct(sales);
 
-      var productName = "";
-      var qty = 0;
+    var productName = "";
+    var qty = 0;
 
-      for(var key in productsSold){  
+    for(var key in productsSold){
         var currentQty = productsSold[key] ;
         if (currentQty > qty) {
             qty = currentQty;
             productName = key;
         };
-      }
-
-      //console.log(productName +','+ qty);
-      //console.log('===========================================================Above productName & qty');
-      return {
-          productName:productName,
-          qty:qty
-      };
-
     }
+
+    //console.log(productName +','+ qty);
+    //console.log('===========================================================Above productName & qty');
+    return {
+        productName:productName,
+        qty:qty
+    };
+
+}
 
 exports.getSaleCostsPerProduct = function(fileName){
 
-      var salesRows = salesFileUtilities.getSales(fileName);
-      var totalSalesPerProduct = salesFileUtilities.getSaleCostsPerProduct(salesRows);
-        // console.log(salesRows);
-      console.log('===========================================================Above salesRows are hidden');
-      return totalSalesPerProduct;
-        // console.log('===========================================================salesRows are hiden');
+    var salesRows = salesFileUtilities.getSales(fileName);
+    var totalSalesPerProduct = salesFileUtilities.getSaleCostsPerProduct(salesRows);
+    // console.log(salesRows);
+    console.log('===========================================================Above salesRows are hidden');
+    return totalSalesPerProduct;
+    // console.log('===========================================================salesRows are hiden');
 
 };
 
-      
-      var qtyPerCategoryMap = {};
 
-      var productCategories = {
-          'Fanta 500ml' : 'Beverages',
-          'Coke 500ml' : 'Beverages',
-          'Cream Soda 500ml' : 'Beverages',
-          'Shampoo 1 litre' : 'Toiletries',
-          'Soap Bar' : 'Toiletries',
-          'Mixed Sweets 5s' : 'Luxuries',
-          'Heart Chocolates' : 'Luxuries',
-          'Iwisa Pap 5kg' : 'Long_Life_Groceries',
-          'Top Class Soy Mince' : 'Long_Life_Groceries',
-          'Bread' : 'Short_Life_Groceries',
-          'Milk 1l' : 'Short_Life_Groceries',
-          'Imasi' : 'Short_Life_Groceries',
-          'Rose (plastic)' : 'Novelty_Goods',
-          'Valentine Cards' : 'Novelty_Goods',
-          'Bananas - loose' : 'Fruit',
-          'Apples - loose' : 'Fruit',
-          'Chakalaka Can' : 'Tinned_Food',
-          'Gold Dish Vegetable Curry Can' : 'Tinned_Food'
-        };
+var qtyPerCategoryMap = {};
 
-  exports.findMostPopularCategory = function(salesFileReader){
+var productCategories = {
+    'Fanta 500ml' : 'Beverages',
+    'Coke 500ml' : 'Beverages',
+    'Cream Soda 500ml' : 'Beverages',
+    'Shampoo 1 litre' : 'Toiletries',
+    'Soap Bar' : 'Toiletries',
+    'Mixed Sweets 5s' : 'Luxuries',
+    'Heart Chocolates' : 'Luxuries',
+    'Iwisa Pap 5kg' : 'Long_Life_Groceries',
+    'Top Class Soy Mince' : 'Long_Life_Groceries',
+    'Bread' : 'Short_Life_Groceries',
+    'Milk 1l' : 'Short_Life_Groceries',
+    'Imasi' : 'Short_Life_Groceries',
+    'Rose (plastic)' : 'Novelty_Goods',
+    'Valentine Cards' : 'Novelty_Goods',
+    'Bananas - loose' : 'Fruit',
+    'Apples - loose' : 'Fruit',
+    'Chakalaka Can' : 'Tinned_Food',
+    'Gold Dish Vegetable Curry Can' : 'Tinned_Food'
+};
 
-        var salesFileReader = salesFileUtilities.getSales("./files/NelisaSalesHistory.csv");
-        var qtyPerProduct = salesFileUtilities.getQtyPerProduct(salesFileReader);
+exports.findMostPopularCategory = function(salesFileReader){
 
-        for(var productName in qtyPerProduct){
-          //console.log('===========================================================Below - productName');
-          //console.log(productName);
-          var qty = qtyPerProduct[productName];
-          var categoryName = productCategories[productName];
-          //console.log('===========================================================Popular categoryName - Below');
-          console.log(categoryName);
+    var salesFileReader = salesFileUtilities.getSales("./files/NelisaSalesHistory.csv");
+    var qtyPerProduct = salesFileUtilities.getQtyPerProduct(salesFileReader);
 
-          if(qtyPerCategoryMap[categoryName] === undefined){
+    for(var productName in qtyPerProduct){
+        //console.log('===========================================================Below - productName');
+        //console.log(productName);
+        var qty = qtyPerProduct[productName];
+        var categoryName = productCategories[productName];
+        //console.log('===========================================================Popular categoryName - Below');
+        console.log(categoryName);
+
+        if(qtyPerCategoryMap[categoryName] === undefined){
             qtyPerCategoryMap[categoryName] = 0;
-          };
-          qtyPerCategoryMap[categoryName] = qtyPerCategoryMap[categoryName] + Number(qty);
-        }
-        console.log('=========================================================== below - Popular CategoryName + Qty PerCategory map');
-        console.log(qtyPerCategoryMap);
-        console.log('=========================================================== end of CategoryName + Qty PerCategory map');
-        
+        };
+        qtyPerCategoryMap[categoryName] = qtyPerCategoryMap[categoryName] + Number(qty);
+    }
+    console.log('=========================================================== below - Popular CategoryName + Qty PerCategory map');
+    console.log(qtyPerCategoryMap);
+    console.log('=========================================================== end of CategoryName + Qty PerCategory map');
 
-        var categoryName = "";
-        var qty = 0;
 
-        for(var key in qtyPerCategoryMap){  
-          var currentQty = qtyPerCategoryMap[key]     
-          if (currentQty > qty) {
+    var categoryName = "";
+    var qty = 0;
+
+    for(var key in qtyPerCategoryMap){
+        var currentQty = qtyPerCategoryMap[key]
+        if (currentQty > qty) {
             qty = currentQty;
             categoryName = key;
-          };  
-        }
-  
-        console.log(categoryName +','+ qty);
-        console.log('=========================================================== Above - Most popular categoryName + qty');
-        return {
-          categoryName: categoryName,
-          qty:qty
         };
-      }
+    }
 
-  exports.findLeastPopularProduct = function(salesFileReader){
+    console.log(categoryName +','+ qty);
+    console.log('=========================================================== Above - Most popular categoryName + qty');
+    return {
+        categoryName: categoryName,
+        qty:qty
+    };
+}
 
-      var sales = salesFileUtilities.getSales("./files/NelisaSalesHistory.csv");
-      var productsSold = salesFileUtilities.getQtyPerProduct(sales);
+exports.findLeastPopularProduct = function(salesFileReader){
 
-      var productName = "";
-      var qty = 10000000;
+    var sales = salesFileUtilities.getSales("./files/NelisaSalesHistory.csv");
+    var productsSold = salesFileUtilities.getQtyPerProduct(sales);
 
-      for(var key in productsSold){  
-        var currentQty = productsSold[key]     
+    var productName = "";
+    var qty = 10000000;
+
+    for(var key in productsSold){
+        var currentQty = productsSold[key]
         if (currentQty < qty) {
             qty = currentQty;
             productName = key;
         };
-      }
-      console.log('===========================================================Above productName & qty');
-      console.log(productName +','+ qty);
-      return {
-          productName:productName,
-          qty:qty
-      };
-
     }
+    console.log('===========================================================Above productName & qty');
+    console.log(productName +','+ qty);
+    return {
+        productName:productName,
+        qty:qty
+    };
 
-  exports.findLeastPopularCategory = function(salesFileReader){
+}
 
-        var salesFileReader = salesFileUtilities.getSales("./files/NelisaSalesHistory.csv");
-        var qtyPerProduct = salesFileUtilities.getQtyPerProduct(salesFileReader);
+exports.findLeastPopularCategory = function(salesFileReader){
 
-        for(var productName in qtyPerProduct){
-          console.log('===========================================================Below - productName');
-          console.log(productName);
-          var qty = qtyPerProduct[productName];
-          var categoryName = productCategories[productName];
-          console.log('=========================================================== categoryName - Below');
-        
-          if(qtyPerCategoryMap[categoryName] === undefined){
-             qtyPerCategoryMap[categoryName] = 0;
-          };
-        }
-        console.log('=========================================================== below - CategoryName + Qty PerCategory map');
-        console.log(qtyPerCategoryMap);
-        var categoryName = "";
-        var qty = 10000000;
+    var salesFileReader = salesFileUtilities.getSales("./files/NelisaSalesHistory.csv");
+    var qtyPerProduct = salesFileUtilities.getQtyPerProduct(salesFileReader);
 
-        for(var key in qtyPerCategoryMap){
-          var currentQty = qtyPerCategoryMap[key];
-          //
-          if (currentQty < qty) {
+    for(var productName in qtyPerProduct){
+        console.log('===========================================================Below - productName');
+        console.log(productName);
+        var qty = qtyPerProduct[productName];
+        var categoryName = productCategories[productName];
+        console.log('=========================================================== categoryName - Below');
+
+        if(qtyPerCategoryMap[categoryName] === undefined){
+            qtyPerCategoryMap[categoryName] = 0;
+        };
+    }
+    console.log('=========================================================== below - CategoryName + Qty PerCategory map');
+    console.log(qtyPerCategoryMap);
+    var categoryName = "";
+    var qty = 10000000;
+
+    for(var key in qtyPerCategoryMap){
+        var currentQty = qtyPerCategoryMap[key];
+        //
+        if (currentQty < qty) {
             qty = currentQty;
             categoryName = key;
-          };
-        }
-        console.log('=========================================================== end of CategoryName + Qty PerCategory map');
-        console.log(categoryName +','+ qty);
-        console.log('=========================================================== Above - Least categoryName + qty');
-        //console.log(productCategories);
-        return {
-          categoryName: categoryName,
-          qty:qty
-        };  
-      };
-//   exports.findMostProfitableProduct = function(salesLines,fileName){
-//     var productsSoldTotal = salesAccessories.getTotalCostPerProduct("./files/NelisaSalesHistory.csv");
-//     var purchasedProductsTotal = salesAccessories.getTotalCostPerProduct()
-//     return productsSoldTotal;
-//       //var purchasedProductsTotal = salesFileUtilities.get
+        };
+    }
+    console.log('=========================================================== end of CategoryName + Qty PerCategory map');
+    console.log(categoryName +','+ qty);
+    console.log('=========================================================== Above - Least categoryName + qty');
+    //console.log(productCategories);
+    return {
+        categoryName: categoryName,
+        qty:qty
+    };
+};
+
+  exports.findMostProfitableProduct = function(salesLines,fileName){
+            var salesLines = salesFileUtilities.getSales(fileName);
+            var productsSoldTotal = salesAccessories.getTotalCostPerProduct(salesLines);
+            console.log(salesLines)
+            var purchasedProductsTotal = salesAccessories.getTotalCostPerProduct(salesLines)
+
+            
+    return productsSoldTotal;
+              //var purchasedProductsTotal = salesFileUtilities.get
+    // var salesRows = salesFileUtilities.getSales(fileName);
+    // var totalSalesPerProduct = salesFileUtilities.getSaleCostsPerProduct(salesRows);
+    // // console.log(salesRows);
+    // console.log('===========================================================Above salesRows are hidden');
+    // return totalSalesPerProduct;
+    // console.log('===========================================================salesRows are hiden');
 
 
 
-// }
+
+}
 
