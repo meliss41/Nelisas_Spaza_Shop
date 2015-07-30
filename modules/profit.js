a2a17e32e57af7826a4c86729c9d602d6ae46b43
oyama <oyama@projectcodex.co>
2015-07-30 13:11:40 +0200

    (parsing)'fileName' parameters are now fixxed & profit is made

--- modules/profit.js
+++ modules/profit.js
@@ -1,15 +1,25 @@
 var totalSalesCost = require("./Nelisa_Spaza_Sales_earnings");
-//var totalPurchSalesCost = require()
-??? sales
-	var totalHistSalesCost = totalSalesCost.getHistTotalCostsPerProduct(sales);
-	console.log(totalSalesCost);
+saleFileProcess = require('./sales_file_utilities');
+// //var totalPurchSalesCost = require()
+// ??? sales
+// 	var totalHistSalesCost = totalSalesCost.getHistTotalCostsPerProduct(sales);
+// 	console.log(totalSalesCost);
 
-??? purchases
-	var totalPurchSalesCost = totalSalesCost.getPurchTotalCostsPerProduct(purchases);
-	console.log(totalPurchSalesCost);
+// ??? purchases
+// 	var totalPurchSalesCost = totalSalesCost.getPurchTotalCostsPerProduct(purchases);
+// 	console.log(totalPurchSalesCost);
+exports.getPurchTotalCostsPerProduct = function(fileName,salesLines){
+
+	var salesLines = saleFileProcess.getSales(fileName);
+	// console.log(salesLines);
+  	var totalSalesPerProduct = totalSalesCost.getPurchTotalCostsPerProduct(salesLines);
+    console.log(salesLines);
+
+    console.log('=========================================================== below - salesRows are hidden');
+    return salesLines;
+    console.log('=========================================================== Above - salesRows are hiden');
 	
-
-
+	};
 
 
 
